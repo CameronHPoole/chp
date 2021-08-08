6b987e61d11f277edcae3949fa2f1017c8479880
CameronHPoole <40720522+CameronHPoole@users.noreply.github.com>
2021-08-08 11:48:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 <div id="text"></div>
 
+<p>Click the button to display a dialog box which will ask for your favorite drink.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
 <script>
+function myFunction() {
+  
+  var tle = window.prompt("Paste your TLE: ");
+  alert("Your TLE is " + tle);
 
-var tle = window.prompt("Paste your TLE: ");
-alert("Your TLE is " + tle);
-
-let str = tle;
-const myArr = tle.split(" ");
+  let str = tle;
+  const myArr = tle.split(" ");
+  
+}
 </script>
